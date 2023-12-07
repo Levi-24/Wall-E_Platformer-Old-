b3521613b19fdb285d5227fbe3013c3bdc439eae
Sufi <matemarcalek@gmail.com>
2023-12-07 15:20:21 +0100

    TransitionFix

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public Animator transition;
     public void MoveToScene(string sceneName)
     {
+        StartCoroutine(LoadLevel(sceneName).GetEnumerator());
+    }
+
+    IEnumerable LoadLevel(string sceneName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1.5f);
+
         SceneManager.LoadScene(sceneName);
     }
 }
