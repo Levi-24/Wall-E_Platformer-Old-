719928c6cf093a6c444346597ea679be927148a9
Sufi <120452154+MarcalekM@users.noreply.github.com>
2023-12-08 22:18:46 +0100

    Update Damage.cs

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -20,7 +20,7 @@ public class Damage : MonoBehaviour
 
     private void Update()
     {
-        if (scoreText.text == "Trash: 13/13" || scoreText.text == "Trash: 15/15" || scoreText.text == "Trash: 19/19")
+        if (scoreText.text == "Trash: 13/13" || scoreText.text == "Trash: 15/15" || scoreText.text == "Trash: 21/21")
         {
             GameManager.Win();
             player.SetActive(false);
