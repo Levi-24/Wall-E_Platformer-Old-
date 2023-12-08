8eb01aeb1c315eb4b850ffcd730f629a5c16ae38
varashus <rigodavid100@gmail.com>
2023-12-08 15:40:23 +0100

    asdad

--- Assets/Scripts/enemypatrol.cs
+++ Assets/Scripts/enemypatrol.cs
@@ -1,23 +1,41 @@
-using UnityEngine;
+    using Unity.VisualScripting;
+    using UnityEngine;
 
-public class Mace : MonoBehaviour
-{
-    public float speed = 0.8f;
-    public float range = 3;
-
-    float startingx;
-    int dir = 1;
-    // Start is called before the first frame update
-    void Start()
+    public class Mace : MonoBehaviour
     {
-        startingx = transform.position.x;
-    }
+        public float speed = 0.8f;
+        public float range = 3;
+        SpriteRenderer spriteRenderer;
+        float startingx;
+        private float sclex;
+        int dir = 1;
+        // Start is called before the first frame update
+        void Start()
+        {
+            startingx = transform.position.x;
+            sclex = transform.localScale.x;
+        }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        transform.Translate(Vector2.right * speed * Time.deltaTime * dir);
-        if (transform.position.x < startingx || transform.position.x > startingx + range)
-            dir *= -1;
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime * dir);
+            if (transform.position.x < startingx || transform.position.x > startingx + range)
+                {
+                    transform.localScale = new Vector3((1)sclex,transform.localScale.y, transform.localScale.z);
+                    dir *= -1;
+                
+                
+                }
+            if (transform.position.x < startingx || transform.position.x > startingx + range)
+            {
+                transform.localScale = new Vector3((-1) * sclex, transform.localScale.y, transform.localScale.z);
+                dir *= 1;
+                
+        }
+        
+    
+
+        }
+   
     }
-}
